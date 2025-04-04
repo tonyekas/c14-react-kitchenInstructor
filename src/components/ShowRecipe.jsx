cd518519dfbf0686df0cf7beeb70a78aaf274524
Tony Enerson <tony@enerson.ca>
2025-04-04 15:48:20 -0600

    Update wording.

--- src/components/ShowRecipe.jsx
+++ src/components/ShowRecipe.jsx
@@ -12,13 +12,13 @@ function NoRecipe() {
         <li>Cut and paste the answer without thinking and move on - pretending you learned something</li>
         <li>Look directly at the answer code, read it and copy/paste the parts you need.</li>
         <li>Refer to REACT documentation or code examples from StackOverflow or GPT. Use those patterns to solve your problem</li>
-        <li>Try your own solution using your memory and creativity, fall back to level 2 if there is a struggle</li>
+        <li>Try your own solution using your memory and creativity, fall back to the previous level when the struggle becomes too great</li>
         <li>Your solutions typipcally work without spending too much time referring to documentation or samples - have a look at the sample code anyway and contrast the approach</li>
-        <li>Try a different way by changing the balance of the component hierarchy, JSX / CSS / JavaScript</li>
+        <li>Creatively come up with multiple differents ways to accomplish your goal by changing the balance of the component hierarchy, JSX / CSS / JavaScript. Choose the ones that fits your situation the best.</li>
       </ol>
       <p>You can test your level again by discarding your changes to the files using git and starting over to see if your approach has changed.</p>
       <p>Today, try to get to operating at level 2-3.</p>
-      <p>If you work with these topics every day, you will reach level 5 for all foundational topics here by the end of the program.  Strive to generally operate at level 3-4 when engaging new problems.</p>
+      <p>If you work with these topics every day, you will reach level 5 for all foundational topics here by the end of the program.  Strive to generally operate at level 4 when engaging new problems.</p>
     </div>
   )
 }
--- src/interaction-onchange/InteractionOnChange.jsx
+++ src/interaction-onchange/InteractionOnChange.jsx
@@ -12,8 +12,9 @@ export default function InteractionOnChange() {
             <p>This function can accept a parameter, which by convention we name <code>event</code> or <code>e</code>.  Note that <code>event.target.value</code> contains the value that the user is typing, or the value that is selected for a select element.</p>
             <p>This is useful because often you will want to hold onto data entered in form fields for validation or other purposes prior to calling APIs to save data, as well as make your front end respond to selections from drop down lists. onChange handlers are the places to update component state.</p>
             <p>Create an onChange handler to update the "name" state variable as the user enters text in the below input.</p>
+            <p>Also, set the "value" prop in the input to the be the value of the "name" state variable so you can see the value as you update it.  This is called creating a "Managed" input.</p>
             <p>The name is: <b>{ name }</b></p>
-            <input className="name-text" value={ name }/>
+            <input className="name-text"/>
         </div>
     )
 }
